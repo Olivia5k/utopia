5e8040bcff2895c628c1f7848338736cb4929e55
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-08-13 11:32:27 +0200

    Add loading of element files into yaml files

--- magic.go
+++ magic.go
@@ -1,7 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 var combinations = make(map[string]Element)
@@ -9,7 +16,7 @@ var elements = make(map[string]Element)
 
 type Element struct {
 	Id   string
-	Name string
+	Name string `yaml:"name"`
 }
 
 // Combine tries to combine two elements and returns a match if possible
@@ -22,28 +29,46 @@ func (e *Element) String() string {
 	return e.Name
 }
 
-// CreateElements will create the test elements
-func CreateElements() {
-	elem := []Element{
-		{"fire", "Fire"},
-		{"ice", "Ice"},
-		{"steam", "Steam"},
+func loadElementFiles(dir string) {
+	fileList := []string{}
+
+	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+		if strings.HasSuffix(path, ".yml") {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
 
-	for _, item := range elem {
-		elements[item.Id] = item
+	for _, file := range fileList {
+		loadElement(file, elements)
+	}
+}
+
+// loadElement loads the yaml file fn and stores the resulting Element{}
+// inside the map argument
+func loadElement(fn string, elements map[string]Element) {
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
 
-	// Make a steam element
-	combinations["fire-ice"] = elem[2]
+	// Extract the filename without the .yml extension
+	id := strings.Split(path.Base(fn), ".")[0]
+
+	elem := Element{Id: id}
+	yaml.Unmarshal(data, &elem)
+	elements[id] = elem
 }
 
 func main() {
-	CreateElements()
+	loadElementFiles("elements/")
 
-	fmt.Println("Hello.")
-	fmt.Printf("Fire is %s\n", elements["fire"])
-	fire := elements["fire"]
-	ice := elements["ice"]
-	fmt.Printf("Combining fire and ice gives: %s\n", fire.Combine(&ice))
+	log.Printf("Elements: %s\n", elements)
+	// fire := elements["fire"]
+	// ice := elements["ice"]
+	// log.Printf("Combining fire and ice gives: %s\n", fire.Combine(&ice))
 }
